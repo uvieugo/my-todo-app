ea0bc8988a221f668cf2283bb2fa929588830129
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2020-06-26 19:54:25 +0100

    Updates

---
+++ precache-manifest.eb21cf97480477c240be5d60cdb7eda0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33599750f3108e294c40164769226752",
+    "url": "/my-todo-app/index.html"
+  },
+  {
+    "revision": "2db58b6e02b89b60af40",
+    "url": "/my-todo-app/static/css/main.2f3b6587.chunk.css"
+  },
+  {
+    "revision": "94c1f23424c25ecc6f34",
+    "url": "/my-todo-app/static/js/2.b0cd81c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-todo-app/static/js/2.b0cd81c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db58b6e02b89b60af40",
+    "url": "/my-todo-app/static/js/main.9666aece.chunk.js"
+  },
+  {
+    "revision": "ffc899d9983c8a74a69f",
+    "url": "/my-todo-app/static/js/runtime-main.f5cbd873.js"
+  }
+]);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import './custom.css'
-
-ReactDom.render(<App />, document.getElementById("root"))
